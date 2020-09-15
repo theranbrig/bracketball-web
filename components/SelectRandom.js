8b173e4ec758c4c15a4609edf871e56ed563d814
Theran <theran.brigowatz@gmail.com>
2020-09-15 20:29:25 +0900

    random pick button and timer

--- components/SelectRandom.js
+++ components/SelectRandom.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { FirebaseActionContext } from '../utilities/context/FirebaseActionContext';
 import Basketball from '../public/icons/basketball.svg';
+
 const SelectRandom = ({ tournament, teams, user }) => {
   const [currentPick, setCurrentPick] = useState(tournament.currentPick);
   const [loading, setLoading] = useState(false);
   const [randomTeamView, setRandomTeamView] = useState('');
+  const [timer, setTimer] = useState('');
+  const [displayTimer, setDisplayTimer] = useState(false);
   const { dbh, firebase } = useContext(FirebaseActionContext);
 
   const displayRandomTeam = () => {
@@ -12,6 +15,9 @@ const SelectRandom = ({ tournament, teams, user }) => {
     const randomTeamNumber = Math.floor(Math.random() * remainingTeams.length);
     setRandomTeamView(remainingTeams[randomTeamNumber].name);
   };
+
+  // const timestamp = Date.now();
+  // console.log(timestamp);
 
   const runDisplayTeams = () => {
     let count = 0;
@@ -26,40 +32,61 @@ const SelectRandom = ({ tournament, teams, user }) => {
     display();
   };
 
-  const selectRandom = () => {
+  const selectRandom = (userId, username) => {
     setLoading(true);
     runDisplayTeams();
-    setTimeout(() => {
+
+    setTimeout(async () => {
       const remainingTeams = teams.filter((team) => team.owner === '');
       const randomIndex = Math.floor(Math.random() * remainingTeams.length);
       const randomTeam = remainingTeams[randomIndex];
-      const timestamp = Date.now();
       dbh
         .collection(`tournaments/${tournament.id}/teams`)
         .doc(randomTeam.id)
-        .update({ owner: user.uid, ownerName: user.username, pick: tournament.currentPick })
-        .then(() => {
+        .update({ owner: userId, ownerName: username, pick: tournament.currentPick })
+        .then(async () => {
+          const timestamp = Date.now();
           dbh
             .collection('tournaments')
             .doc(tournament.id)
-            .update({ currentPick: tournament.currentPick + 1, previousPickTime: timeStamp })
+            .update({ currentPick: tournament.currentPick + 1, previousPickTime: timestamp })
             .then(() => {
               setLoading(false);
               setRandomTeamView('');
-            });
+            })
+            .catch((err) => console.log(err));
         })
         .catch((err) => console.log(err));
     }, 5500);
   };
 
+  const runTimer = () => {
+    setInterval(() => {
+      const timestamp = Date.now();
+      setTimer(60 - Math.floor((timestamp - tournament.previousPickTime) / 1000));
+      if (60 - Math.floor((timestamp - tournament.previousPickTime) / 1000) === 0) {
+        if (user.role === 'OWNER') {
+          setDisplayTimer(false);
+          selectRandom(currentPick.id, currentPick.username);
+        }
+      }
+    }, 1000);
+  };
+
   useEffect(() => {
     setCurrentPick(tournament.picks[tournament.currentPick - 1]);
+
+    if (currentPick && currentPick.id === user.uid) {
+      setDisplayTimer(true);
+      runTimer();
+    }
   }, [tournament]);
 
   return (
     <>
       {currentPick && currentPick.id === user.uid ? (
         <div className='flex flex-col justify-center items-center mt-12'>
+          {timer >= 0 && timer < 90 ? <h2 className='font-number'>{timer}</h2> : null}
           <div className='random-team'>
             {randomTeamView.length ? (
               <h2 className='text-center text-2xl'>
@@ -79,9 +106,8 @@ const SelectRandom = ({ tournament, teams, user }) => {
           </div>
           <button
             className='bg-celadon text-honeydew font-title px-8 py-2 rounded-full mt-4 border-prussian border-2 hover:bg-celadonDark active:bg-celadonDark duration-500 disabled:opacity-75'
-            disabled={loading}
-            onClick={() => {
-              selectRandom();
+            onClick={async () => {
+              await selectRandom(user.uid, user.username);
             }}>
             Select Random Team
           </button>
