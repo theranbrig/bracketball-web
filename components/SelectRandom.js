9f4e46675a381a59abd1c66e790ef1b5da35da40
Theran <theran.brigowatz@gmail.com>
2020-09-16 10:30:12 +0900

    timer component

--- components/SelectRandom.js
+++ components/SelectRandom.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { FirebaseActionContext } from '../utilities/context/FirebaseActionContext';
 import Basketball from '../public/icons/basketball.svg';
+import Timer from './Timer';
+import { createErrorToast } from '../utilities/toast';
 
 const SelectRandom = ({ tournament, teams, user }) => {
   const [currentPick, setCurrentPick] = useState(tournament.currentPick);
   const [remainingTeams, setRemainingTeams] = useState([]);
   const [loading, setLoading] = useState(false);
   const [randomTeamView, setRandomTeamView] = useState('');
-  const [timer, setTimer] = useState('');
-  const [displayTimer, setDisplayTimer] = useState(false);
+
   const { dbh, firebase } = useContext(FirebaseActionContext);
 
   const displayRandomTeam = async () => {
@@ -16,8 +17,8 @@ const SelectRandom = ({ tournament, teams, user }) => {
     setRandomTeamView(remainingTeams[randomTeamNumber].name);
   };
 
-  const timestamp = Date.now();
-  console.log(timestamp);
+  // const timestamp = Date.now();
+  // console.log(timestamp);
 
   const runDisplayTeams = () => {
     let count = 0;
@@ -32,65 +33,58 @@ const SelectRandom = ({ tournament, teams, user }) => {
     display();
   };
 
-  const updatePick = (userId, username, randomTeam) => {
-    setLoading(true);
-    dbh
-      .collection(`tournaments/${tournament.id}/teams`)
-      .doc(randomTeam.id)
-      .update({ owner: userId, ownerName: username, pick: tournament.currentPick })
-      .then(async () => {
-        const timestamp = Date.now();
-        dbh
-          .collection('tournaments')
-          .doc(tournament.id)
-          .update({ currentPick: tournament.currentPick + 1, previousPickTime: timestamp })
-          .then(() => {
-            setLoading(false);
-            setRandomTeamView('');
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const selectRandom = async (userId, username) => {
-    setLoading(true);
-    const randomIndex = Math.floor(Math.random() * remainingTeams.length);
-    const randomTeam = remainingTeams[randomIndex];
-    console.log(randomTeam);
-    if (remainingTeams.length) {
-      runDisplayTeams();
-      setTimeout(() => {
-        updatePick(user.uid, user.username, randomTeam);
-      }, 5500);
+  const makePick = async (userId, username, loadingCallback) => {
+    loadingCallback(true);
+    const pickableTeams = await teams.filter((team) => team.owner === '');
+    const randomIndex = await Math.floor(Math.random() * pickableTeams.length);
+    const randomTeam = await pickableTeams[randomIndex];
+    if (pickableTeams.length) {
+      await dbh
+        .collection(`tournaments/${tournament.id}/teams`)
+        .doc(randomTeam.id)
+        .update({ owner: userId, ownerName: username, pick: tournament.currentPick })
+        .then(async () => {
+          const timestamp = await Date.now();
+          await dbh
+            .collection('tournaments')
+            .doc(tournament.id)
+            .update({ currentPick: tournament.currentPick + 1, previousPickTime: timestamp })
+            .then(() => {
+              loadingCallback(false);
+              setRandomTeamView('');
+            })
+            .catch((err) => {
+              console.log(err);
+              loadingCallback(false);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+          loadingCallback(false);
+        });
     } else {
       console.log('NO TEAMS');
+      loadingCallback(false);
+      createErrorToast('No Teams Remaining');
     }
   };
 
-  const runTimer = () => {
-    setInterval(() => {
-      const timestamp = Date.now();
-      setTimer(60 - Math.floor((timestamp - tournament.previousPickTime) / 1000));
-      if (60 - Math.floor((timestamp - tournament.previousPickTime) / 1000) === 0) {
-        if (user.role === 'OWNER') {
-          const randomIndex = Math.floor(Math.random() * remainingTeams.length);
-          const randomTeam = remainingTeams[randomIndex];
-          updatePick(currentPick.id, currentPick.username, randomTeam);
-        }
-      }
-    }, 1000);
+  const selectRandomClick = async (userId, username) => {
+    setLoading(true);
+    runDisplayTeams();
+    setTimeout(() => {
+      makePick(user.uid, user.username, randomTeam, setLoading);
+    }, 5500);
   };
 
   useEffect(() => {
     setCurrentPick(tournament.picks[tournament.currentPick - 1]);
     setRemainingTeams(teams.filter((team) => team.owner === ''));
-    runTimer();
   }, [tournament]);
 
   return (
     <>
-      {timer >= 0 && timer < 90 ? <h2 className='font-number'>{timer}</h2> : null}
+      <Timer tournament={tournament} currentPick={currentPick} user={user} makePick={makePick} />
       {currentPick && currentPick.id === user.uid ? (
         <div className='flex flex-col justify-center items-center mt-12'>
           <div className='random-team'>
@@ -113,7 +107,7 @@ const SelectRandom = ({ tournament, teams, user }) => {
           <button
             className='bg-celadon text-honeydew font-title px-8 py-2 rounded-full mt-4 border-prussian border-2 hover:bg-celadonDark active:bg-celadonDark duration-500 disabled:opacity-75'
             onClick={async () => {
-              await selectRandom(user.uid, user.username );
+              await selectRandomClick(user.uid, user.username);
             }}>
             Select Random Team
           </button>
---
+++ components/Timer.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = ({ tournament, user, currentPick, makePick }) => {
+  const [currentCount, setCount] = useState(60);
+  const [loading, setLoading] = useState(false);
+
+  const timer = () => {
+    const timestamp = Date.now();
+    setCount(60 - Math.floor((timestamp - tournament.previousPickTime) / 1000));
+  };
+
+  useEffect(() => {
+    console.log(tournament);
+    if (currentCount === 0) {
+      if (user.role !== 'OWNER' && tournament.currentPick <= tournament.picks.length && !loading) {
+        console.log('CALLED');
+        makePick(currentPick.id, currentPick.username, setLoading);
+        return;
+      } else {
+        return;
+      }
+    }
+    const id = setInterval(timer, 1000);
+    return () => clearInterval(id);
+  }, [currentCount, tournament.previousPickTime]);
+
+  return <div>{currentCount}</div>;
+};
+
+export default Timer;
