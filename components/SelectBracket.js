ac05cc81047ba421790acd9d5af916b93755ed69
Theran <theran.brigowatz@gmail.com>
2020-09-24 15:35:25 +0900

    more bracket changes

--- components/SelectBracket.js
+++ components/SelectBracket.js
@@ -23,29 +23,29 @@ const SelectBracket = ({ teams, tournament }) => {
         groupTeams,
       };
     });
-    console.log(groups);
     setBracketGroups(groups);
     setLoading(false);
   };
 
   useEffect(() => {
     selectTeams();
-  }, []);
+  }, [teams]);
 
   if (loading) return <p>Loading</p>;
+
   return (
-    <div className='overflow-y-scroll'>
+    <div className='overflow-y-scroll mt-4'>
       {bracketGroups.length &&
         bracketGroups.map((group, idx) => {
           return (
             <div className=''>
-              <div className={`flex flex-row p-2 ${idx % 2 !== 0 ? 'bg-celadon' : 'bg-prussian'}`}>
-                <h4>{group.group}</h4>
+              <div className={`flex flex-row p-2 ${idx % 2 !== 0 ? 'bg-celadon' : 'bg-powder'}`}>
+                <h4 className='group-title bg-honeydew px-6 py-1 text-prussian rounded-md text-sm border-2 border-prussian'>{group.group}</h4>
                 <ul className='w-full flex flex-col justify-between'>
                   {group.groupTeams.map((team) => {
                     return (
-                      <button className='bg-powder text-prussian p-1 rounded-full pl-4 my-2  border-2 border-honeydew text-left hover:bg-powderDark'>
-                        {team.seed} - {team.name}
+                      <button className='bg-prussian text-honeydew p-1 rounded-full pl-4 my-2  border-2 border-honeydew text-left hover:bg-prussianDark hover:shadow-lg shadow-honeydew'>
+                        {team ? `${team.seed} - ${team.name}` : null}
                       </button>
                     );
                   })}
